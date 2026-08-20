8dca3ac1675538159baf6fbde9eef22cfb89ee47
Akshit Ramola <ramolaakshit1@gmail.com>
2026-08-21 03:43:51 +0530

    fix(ai-orchestration): set package type to module and fix tool parameter destructuring

---
+++ ai-orchestration/src/agents/tools.js
@@ -0,0 +1,73 @@
+import axios from 'axios';
+import { tool } from "langchain";
+import * as z from "zod";
+
+export const listFiles = tool(
+    async ({ }) => {
+        console.log("========================================");
+        console.log("using list_files tool");
+        console.log("========================================");
+        const response = await axios.get("http://01a01ba2-aa65-77f0-9a54-a7614717ee46.agent.localhost/list-files");
+
+        console.log("========================================");
+        console.log("response from list file tools", response.data);
+        console.log("========================================");
+        return JSON.stringify(response.data.files);
+    },
+    {
+        name: "list_files",
+        description: "List all files in the project directory. This is useful for understanding what files are available to work with.",
+        inputSchema: z.object({})
+    }
+)
+
+export const readFiles = tool(
+
+    async ({ files }) => {
+        console.log("========================================");
+        console.log("using read_files tool", files);
+        console.log("========================================");
+
+        const response = await axios.get("http://01a01ba2-aa65-77f0-9a54-a7614717ee46.agent.localhost/read-files?files=" + files.join(","))
+
+        console.log("========================================");
+        console.log("response from read file tools", response.data);
+        console.log("========================================");
+
+        return JSON.stringify(response.data);
+    },
+    {
+        name: "read_files",
+        description: "Read the contents of specified files. This is useful for understanding the contents of files that are relevant to the task at hand.",
+        inputSchema: z.object({
+            files: z.array(z.string()).describe("The list of files absolute paths to read. These should be files that were listed using the list_files tool or created later")
+        })
+    }
+)
+
+export const updateFiles = tool(
+    async ({ files }) => {
+        console.log("========================================");
+        console.log("using update_files tool", files);
+        console.log("========================================");
+        const response = await axios.post("http://01a01ba2-aa65-77f0-9a54-a7614717ee46.agent.localhost/update-files", {
+            updates: files
+        });
+
+        console.log("========================================");
+        console.log("response from update file tools", response.data);
+        console.log("========================================");
+
+        return JSON.stringify(response.data.results);
+    },
+    {
+        name: "update_files",
+        description: "Update the contents of specified files. This is useful for making changes to files based on the requirements of the task at hand. This tool can also use to create new files by providing a new file name in the file field and the content to be added in the content field.",
+        inputSchema: z.object({
+            files: z.array(z.object({
+                file: z.string().describe("The absolute path of the file to update"),
+                content: z.string().describe("The new content for the file")
+            })).describe("The list of files to update and their new contents")
+        })
+    }
+)
